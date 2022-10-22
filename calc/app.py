c84d0dfc69d7a6792f71b276da9cbe7991838c8a
joshua <joshua.r.foster3@gmail.com>
2022-10-22 17:52:40 -0400

    complete

---
+++ calc/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_numbers():
+    """Add a and b"""
+    a=int(request.args.get('a'))
+    b=int(request.args.get('b'))
+    result=add(a,b)
+    return str(result)
+
+@app.route('/sub')
+def sub_numbers():
+    """Subtract a and b"""
+    a=int(request.args.get('a'))
+    b=int(request.args.get('b'))
+    result=sub(a,b)
+    return str(result)
+
+@app.route('/mult')
+def mult_numbers():
+    """Multiply a and b"""
+    a=int(request.args.get('a'))
+    b=int(request.args.get('b'))
+    result=mult(a,b)
+    return str(result)
+
+@app.route('/div')
+def div_numbers():
+    """Divide a and b"""
+    a=int(request.args.get('a'))
+    b=int(request.args.get('b'))
+    result=div(a,b)
+    return str(result)
